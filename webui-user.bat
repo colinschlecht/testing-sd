24ecae56292dec8006ac657d9340f70293d3965c
Colin Schlecht <schlechtcolin@yahoo.com>
2024-03-29 03:57:03 -0700

    adds listen and share

--- webui-user.bat
+++ webui-user.bat
@@ -7,7 +7,7 @@ set PYTHON=py
 set GIT=git
 set VENV_DIR=
 @REM set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --enable-insecure-extension-access
-set COMMANDLINE_ARGS=--medvram --no-half --precision full --skip-torch-cuda-test --enable-insecure-extension-access --server-name="0.0.0.0" --share
+set COMMANDLINE_ARGS=--medvram --no-half --precision full --skip-torch-cuda-test --enable-insecure-extension-access --listen --share
 
 set PYTORCH_TRACING_MODE=TORCHFX
 set USE_OPENVINO=1
