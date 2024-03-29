d8ba947d19ef618459dd2aea9b37019f6d73a927
colinschlecht <schlechtcolin@yahoo.com>
2024-03-29 00:42:42 -0700

    adds bat file

---
+++ webui-user.bat
@@ -0,0 +1,16 @@
+
+@echo off
+
+set SHARE=true
+
+set PYTHON=py
+set GIT=git
+set VENV_DIR=
+@REM set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --no-half --enable-insecure-extension-access
+set COMMANDLINE_ARGS=--medvram --no-half --precision full --skip-torch-cuda-test --enable-insecure-extension-access --server-name="0.0.0.0"y
+
+set PYTORCH_TRACING_MODE=TORCHFX
+set USE_OPENVINO=1
+
+call webui.bat
+
